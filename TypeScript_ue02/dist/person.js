d9339118129302f2ef0ebec2ce5c767340a91683
rufflm14 <33861122+rufflm14@users.noreply.github.com>
2018-10-12 09:48:45 +0200

    Add files via upload

---
+++ TypeScript_ue02/dist/person.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(vorname, nachname, birthYear) {
+        this._vorname = vorname;
+        this._nachname = nachname;
+        this._birthYear = birthYear;
+    }
+    get vorname() {
+        return this.vorname;
+    }
+    get nachname() {
+        return this.nachname;
+    }
+    get birthYear() {
+        return this.birthYear;
+    }
+}
+exports.Person = Person;
+
+//# sourceMappingURL=person.js.map
