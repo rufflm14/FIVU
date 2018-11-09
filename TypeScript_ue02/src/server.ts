348e680e0e6cbe22151b073cf2d6e68ed415d04e
rufflm14 <rufflm14@htlkaindorf.at>
2018-11-09 11:52:27 +0100

    09.11.2018

--- TypeScript_ue02/src/server.ts
+++ TypeScript_ue02/src/server.ts
@@ -1,7 +1,9 @@
 
 // Node.js Modul
-import * as http from 'http';
-// Express als externens Modul
+// import * as http from 'http';
+import * as path from 'path';
+
+// Externes Modul
 import * as express from 'express';
 
 export class Server {
@@ -9,9 +11,11 @@ export class Server {
     private _port: number;
     private _server: express.Express;
 
-    constructor (port: number) {
+    public constructor (port: number) {
         this._port = port;
         this._server = express();
+        this._server.get('/liste', (req, res, next) => this.handleGetListe(req, res, next));
+        this._server.get('/liste', (req, res, next) => this.sendImage(res));
     }
 
     public start () {
@@ -21,5 +25,16 @@ export class Server {
 
     public get port () {
         return this._port;
+    }
+
+    private handleGetListe (req: express.Request, res: express.Response, next: express.NextFunction) {
+       const filePath = path.join(__dirname, '..', 'assets', 'liste.html');
+       console.log(filePath);
+       res.sendFile(filePath);
+    }
+
+    private sendImage(res: express.Response) {
+        const filePath = path.join(__dirname. '..', 'assets', 'image.png');
+        res.sendFile(filePath);
     }
 }
