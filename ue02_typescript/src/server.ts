c774533e1067dfe11aeb812b1692597af1ccfa3c
rufflm14 <rufflm14@htlkaindorf.at>
2018-11-30 10:03:09 +0100

    SX-Stuff

---
+++ ue02_typescript/src/server.ts
@@ -0,0 +1,49 @@
+
+// Node.js Modul
+// import * as http from 'http';
+import * as path from 'path';
+
+// Externes Modul (via npm instaliieren)
+import * as express from 'express';
+
+export class Server {
+
+    private _port: number;
+    private _server: express.Express;
+
+    public constructor (port: number) {
+        const assetsPath = path.join(__dirname, '..', 'assets');
+        this._port = port;
+        this._server = express();
+        this._server.use('/', express.static(assetsPath));
+        this._server.get('/liste',
+          (req, res, next) => this.handleGetListe(req, res, next)
+        );
+        this._server.get('/image.png',
+          (req, res, next) => this.sendImage(res)
+        );
+
+    }
+
+    public start () {
+        this._server.listen(this._port);
+        console.log('HTTP server gestartet auf port ' + this._port);
+    }
+
+    public get port () {
+        return this._port;
+    }
+
+    private handleGetListe (req: express.Request, res: express.Response,
+                            next: express.NextFunction) {
+        // res.send('Guten Morgen, Herr Muri');
+        const filePath = path.join(__dirname, '..', 'assets', 'liste.html');
+        console.log(filePath);
+        res.sendFile(filePath);
+    }
+
+    private sendImage (res: express.Response) {
+        const filePath = path.join(__dirname, '..', 'assets', 'image.png');
+        res.sendFile(filePath);
+    }
+}
